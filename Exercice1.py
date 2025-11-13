e9c6346faf89ca4e3eec0c44caf882db347f9f82
howlyc <howlyc@educ.college-montreal.qc.ca>
2025-11-13 16:12:17 -0500

    Exercice 4: Hero

---
+++ Exercice1.py
@@ -0,0 +1,80 @@
+from math import pi
+
+"""
+EXERCICE 1
+"""
+class StringFoo:
+    def __init__(self):
+        self.message = "initialiser à rien"
+
+    def set_string(self, msg):
+        self.message = msg
+
+    def print_string(self):
+        print(self.message.upper())
+
+
+str1 = StringFoo()
+str1.print_string()
+str1.set_string("Allo")
+str1.print_string()
+
+"""
+EXERCICE 2
+"""
+class Rectangle:
+    def __init__(self, longueur, largeur):
+        self.longueur = longueur
+        self.largeur = largeur
+        self.aire = 0
+
+    def calcul_aire(self):
+        self.aire = self.longueur * self.largeur
+
+    def afficher_infos(self):
+        print(self.aire)
+
+
+r = Rectangle(10, 9)
+r.calcul_aire()
+r.afficher_infos()
+
+"""
+EXERCICE 3
+"""
+class Cercle:
+    def __init__(self, rayon):
+        self.rayon = rayon
+        self.aire = 0
+        self.circonference = 0
+
+    def calcul_aire(self):
+        self.aire = pi * self.rayon ** 2
+
+    def calcul_circonference(self):
+        self.circonference = 2 * pi * self.rayon
+
+    def afficher_infos(self):
+        print(self.aire)
+        print(self.circonference)
+
+
+c = Cercle(5)
+c.calcul_aire()
+c.calcul_circonference()
+c.afficher_infos()
+
+"""
+EXERCICE 4
+"""
+class Hero:
+    def __init__(self, vie, attaque, defense, nom):
+        self.vie = vie
+        self.attaque = attaque
+        self.defense = defense
+        self.nom = nom
+
+    def faire_attaque(self):
+
+
+hero = Hero(5, 1, 1, "Caesar")
