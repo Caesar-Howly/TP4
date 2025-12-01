589f68f31756e12719d257cb8141baa59f4e422a
howlyc <howlyc@educ.college-montreal.qc.ca>
2025-12-01 09:58:37 -0500

    Fini

---
+++ vgu.py
@@ -0,0 +1,8 @@
+class MonObjet:
+    def __init__(self, message_initial):
+        self.message = message_initial  # Assigne le paramètre à l'attribut
+
+
+# Exemple d'utilisation
+mon_objet = MonObjet("Ceci est un message.")
+print(mon_objet.message)
